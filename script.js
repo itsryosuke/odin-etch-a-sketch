49f00aa094cce94545c0a267a513e3c306ae4912
itsryosuke <155581789+itsryosuke@users.noreply.github.com>
2025-03-26 20:31:15 +0100

    Add button that changes the size of the grid

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
 const container = document.querySelector("#container");
 
-function makeGrid() {
-    for (i = 0; i < 16; i++) {
+function makeGrid(gridSize) {
+    for (i = 0; i < gridSize; i++) {
         const column = document.createElement("div");
         column.className = "column";
         container.appendChild(column);
 
-        for (j = 0; j < 16; j++) {
+        for (j = 0; j < gridSize; j++) {
             const row = document.createElement("div");
             row.className = "row";
             column.appendChild(row);
         }
     }
-}
-
-makeGrid();
-
-const row = document.querySelectorAll(".row");
+    const row = document.querySelectorAll(".row");
 
 row.forEach(function(mySquare){
     mySquare.addEventListener("mouseenter", function(e) {
         e.target.style.backgroundColor = "red";
     })
 })
+}
 
+makeGrid(16);
+
+
+document.getElementById("size").onclick = () => {
+    let userInput = Number(prompt("Please enter the number of squares per side for the new grid\n\n(limit - 100)"));
+    if (userInput > 0 && userInput <= 100) {
+        const columns = document.querySelectorAll(".column");
+        columns.forEach(function(column){
+            column.remove();
+        })
+        makeGrid(userInput);
+    }
+    else {
+        alert("Invalid number!\n\nChoose a number between 1 and 100")
+    }
+}
